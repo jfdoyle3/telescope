dca25f4e5afba7cd71ef7c3644d304d015ac95cd
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2024-05-27 09:45:03 -0400

    updated controller. simple static compass console response

--- controller.py
+++ controller.py
@@ -7,30 +7,39 @@ North: 90  East: 90 | South: 180  | West: 270
 degrees=0
 drgInc=1
 counter=0
-userInput=0
 
-while degrees<360:
-    """
+
+userInput=int(input("Enter degrees "))
+
+""" 
     if degrees == userInput :
         print(userInput,"!!")
         break
-    """
+"""
+
+
+while degrees<360:
+
     if degrees==0:
             print("North")
+            
             
     elif degrees==90:
             print("East")
             
+            
     elif degrees==180:
             print("South")
+            
 
     elif degrees==270:
             print("West")
+            
             
     else:
             print(degrees)
     counter+=1
     degrees+=drgInc
-    
+   
     
 print("End of Line")
