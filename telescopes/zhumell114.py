8bf7ad98504e3990c74e946ed8ab18ce72269b8a
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2024-05-27 10:11:20 -0400

    added a telescope object

---
+++ telescopes/zhumell114.py
@@ -0,0 +1,16 @@
+"""
+ Zhumell 114 - https://zhumell.com/products/zhumell-z114-portable-altazimuth-reflector-telescope
+    Objective/Aperture        114 mm / 4.5”
+    Focal Length              450 mm
+    Limiting Magnitude        12.98
+    Focal Ratio               f/3.95
+    Eyepiece Format           1.25”
+    Finderscope               Red Dot Finder
+    Mount Type                Tabletop altitudeazimuth / Dobsonian
+    Tube Mount                Tube clamp 
+    Mount Adjustments         Altitude-azimuth Altitude-azimuth
+    Materials Wood,           Melamine Wood
+"""
+
+class zhumell114
+    def __init__(self)
