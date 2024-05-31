93b325e93e4d6b12e6e436a5c586fc393fdb4b14
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2024-05-30 22:32:20 -0400

    added formulas

--- objects/lens.py
+++ objects/lens.py
@@ -1,31 +1,65 @@
 class lens:
     def __init__(self,name,magnification):
         self.name=name
-        self.magnification=magnification
+        self.focalLength=magnification
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ """       
+
+    def baseLensMagnification(self,lensFocalLength):
+        baseLensFocalLength=self.focalLength/lensFocalLength
+        return baseLensFocalLength
+    
+    def baseBarlowX2Magnification(self):
+        baseBarlowX2Magnification=self.focalLength*2
+        return baseBarlowX2Magnification
+
+    def lensBarlowX2Magnification(self,lensFocalLength):
+        lensBarlowX2Magnification=self.focalLength*2/lensFocalLength
+        return lensBarlowX2Magnification
+
+    def highestUsefulMagnification(self):
+        highestMagnification=self.aperture*30
+        return highestMagnification
+
+
+
+# Focal Length
+print("Focal Length: "+str(telescope.focalLength)+"mm")
+
+
 
 
 # Base Lens Magnification
-def base(focal)
 baseLensMagnification=telescope.baseLensMagnification(lens)
 print("Focal length at "+str(telescope.focalLength)+"mm with a "+str(lens)+"mm is "+str(round(baseLensMagnification))+"X magnification.")
 
 
-
-"""
-# Focal Length
-print("Focal Length: "+str(telescope.focalLength)+"mm")
 
 #Barlow X2 Lens Effective Magnification
 baseBarlowX2Magnification=telescope.baseBarlowX2Magnification()
 print("Barlow X2 Lens Effective Magnification: "+str(baseBarlowX2Magnification)+"mm")
 
-# Base Lens Magnification
-baseLensMagnification=telescope.baseLensMagnification(lens)
-print("Focal length at "+str(telescope.focalLength)+"mm with a "+str(lens)+"mm is "+str(round(baseLensMagnification))+"X magnification.")
+
 
 # Barlow X2 Lens Magnification
 lensBarlowX2Magnification=telescope.lensBarlowX2Magnification(lens)
 print("Focal length at "+str(telescope.focalLength)+"mm with a "+str(lens)+"mm Barlow X2 gives a "+str(round(lensBarlowX2Magnification))+"X magnification.")
+
+
 
 # Highest Useful Magnification
 print("\nHighest Useful Magnification (x30): "+str(round(telescope.highestUsefulMagnification()))+"X on normal weather conditions.")
--- objects/zhumell114.py
+++ objects/zhumell114.py
@@ -19,3 +19,18 @@ class Zhumell114:
         self.aperture = 4.5
         self.focalRatio=3.95
     
+    def baseLensMagnification(self,lensFocalLength):
+        baseLensFocalLength=self.focalLength/lensFocalLength
+        return baseLensFocalLength
+    
+    def baseBarlowX2Magnification(self):
+        baseBarlowX2Magnification=self.focalLength*2
+        return baseBarlowX2Magnification
+
+    def lensBarlowX2Magnification(self,lensFocalLength):
+        lensBarlowX2Magnification=self.focalLength*2/lensFocalLength
+        return lensBarlowX2Magnification
+
+    def highestUsefulMagnification(self):
+        highestMagnification=self.aperture*30
+        return highestMagnification
